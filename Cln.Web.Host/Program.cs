ef3370be2a49cfc38c969c6ee71bd89f15eb183d
ken.jones <ken.jones@tightlines.io>
2020-10-25 14:21:13 -0400

    update core and lib verions

--- Cln.Web.Host/Program.cs
+++ Cln.Web.Host/Program.cs
@@ -1,26 +1,28 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace Cln.Web.Api
 {
-    public sealed class Program
+  public sealed class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Seed().Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+      CreateHostBuilder(args).Build().Run();
     }
 
-    public static class DatabaseSeedInitializer
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+            });
+  }
+
+  public static class DatabaseSeedInitializer
+  {
+    public static IWebHost Seed(this IWebHost host)
     {
-        public static IWebHost Seed(this IWebHost host)
-        {
-            return host;
-        }
+      return host;
     }
+  }
 }
